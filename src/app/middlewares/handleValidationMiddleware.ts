195d249a74754f833945619871a8b55b07d53b71
iMatheusPaula <imatheuspaula@gmail.com>
2025-09-20 01:17:29 -0300

    fix handleValidationMiddleware

--- src/app/middlewares/handleValidationMiddleware.ts
+++ src/app/middlewares/handleValidationMiddleware.ts
@@ -1,10 +1,13 @@
-import { Request, Response, NextFunction } from 'express';
-import { validationResult } from 'express-validator';
+import {Request, Response, NextFunction} from 'express';
+import {validationResult} from 'express-validator';
 
 export const handleValidation = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
+    res.status(422).json({errors: errors.array()});
+    return;
   }
+
   next();
 };
